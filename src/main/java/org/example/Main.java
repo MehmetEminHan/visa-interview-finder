1432631e5ef7a9461a5b0e0ccc5475e982bc9868
MehmetEminHan <biyikmehmetemin@gmail.com>
2024-08-25 22:19:51 -0400

    Created loop algorithm

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -28,7 +28,25 @@ public class Main {
 
     public static void main(String[] args) throws InterruptedException {
 
-        pageUtils.runLogin("5","2026");
+        int retryCount = 100;
+        int attempt = 0;
+
+        while (attempt < retryCount) {
+            try {
+                pageUtils.runLogin("4","2025");
+            } catch (Exception e) {
+                attempt++;
+                // If the max retry count is reached, print the exception
+                if (attempt == retryCount) {
+                    System.out.println("Max retries reached. Exception: " + e.getMessage());
+                    e.printStackTrace();
+                }
+            }
+        }
+
+
+
+
     }
 
 
